2b9d6825184b8ffba141e73720d7a76a718f1fc9
ilynne <ilynne@gmail.com>
2015-05-28 17:32:07 -0700

    refactor int_to_array

--- euler.rb
+++ euler.rb
@@ -4,15 +4,29 @@ require 'rspec'
 
 class Euler
 
+  def problem_16(num, power)
+    num ** power
+  end
+
   def problem_20(num)
     factorial = num.downto(1).inject(:*)
+    digits = int_to_array(factorial)
+    reduce_digits(digits)
+  end
+
+  def int_to_array(int)
     digits = []
-    while factorial != 0 do
-      factorial, last_digit = factorial.divmod(10)
+    while int != 0 do
+      int, last_digit = int.divmod(10)
       digits << last_digit
     end
-    digits.reduce(:+)
+    digits
   end
+
+  def reduce_digits(arr)
+    arr.reduce(:+)
+  end
+
 end
 
 describe 'Project Euler Problems' do
@@ -20,9 +34,16 @@ describe 'Project Euler Problems' do
     @euler = Euler.new
   end
 
-  it 'should do something' do
-    expect(@euler.problem_20(10)).to eq(27)
-    expect(@euler.problem_20(9)).to eq(27)
-    expect(@euler.problem_20(100)).to eq(648)
+  describe 'problem 16' do
+    it 'should return the sum of the power result digits' do
+      expect(@euler.problem_16(2, 15)).to eq(26)
+    end
+  end
+  describe 'problem 20' do
+    it 'should return the sum of the factorial result digits' do
+      expect(@euler.problem_20(10)).to eq(27)
+      expect(@euler.problem_20(9)).to eq(27)
+      expect(@euler.problem_20(100)).to eq(648)
+    end
   end
 end
